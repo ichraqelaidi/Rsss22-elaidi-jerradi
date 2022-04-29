7603aed9441b5a011aa954ed949edc869c7b5692
ichraq elaidi <ichraqelaidi55@gmail.com>
2022-04-30 00:02:56 +0200

    first commit

---
+++ src/main/java/fr/univrouen/rss22/Rss22ElaidiJerradiApplication.java
@@ -0,0 +1,14 @@
+package fr.univrouen.rss22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rss22ElaidiJerradiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("heloo testtt");
+		SpringApplication.run(Rss22ElaidiJerradiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univrouen/rss22/Rss22ElaidiJerradiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rss22ElaidiJerradiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
